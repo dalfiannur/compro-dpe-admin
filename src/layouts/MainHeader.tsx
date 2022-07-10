c3b543248d06d13baa2e4c1b4b0831fec2ffcab1
Shafly Hamzah <shafly2000@gmail.com>
2022-07-10 19:42:23 +0700

    add logo

--- src/layouts/MainHeader.tsx
+++ src/layouts/MainHeader.tsx
@@ -1,6 +1,8 @@
 import React, {FC} from "react";
 import {Burger, Header, MediaQuery, Text, useMantineTheme} from "@mantine/core";
 
+import logo from "../assets/logo-blue.svg"; 
+
 interface MainHeaderProp {
   opened: boolean;
   setOpened: (value: boolean) => void;
@@ -35,7 +37,10 @@ export const MainHeader: FC<MainHeaderProp> = ({opened, setOpened}) => {
             mr="xl"
           />
         </MediaQuery>
-        <Text>Application header</Text>
+        <div>
+            <img src={logo} height="48" style={{margin: 25}}/>
+        </div>
+        <Text>Website Admin Panel</Text>
       </div>
     </Header>
   )
